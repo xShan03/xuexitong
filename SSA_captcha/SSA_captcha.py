164ce6213c4f96811f30dc99054e6892f14f717d
Doone <64115840+Doone-skser@users.noreply.github.com>
2022-03-21 18:40:48 +0800

    新版支持验证码

---
+++ SSA_captcha/SSA_captcha.py
@@ -0,0 +1,206 @@
+import random
+import threading
+import time
+from datetime import date, timedelta
+import re
+import requests
+import base64
+import json
+import captchaCv
+
+
+class CX:
+    def __init__(self, phonenums, password, roomid, seatnums):
+        self.acc = phonenums
+        self.pwd = password
+        self.roomid = roomid
+        self.seatnums = seatnums
+        self.day = (date.today() + timedelta(days=+1)).strftime("%Y-%m-%d")
+        self.token = None
+        self.pageToken = None
+        self.captcha = None
+        self.session = requests.session()
+        self.session.headers = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Mobile Safari/537.36',
+            'sec-ch-ua-platform': '"Android"',
+            'Referer': 'https://office.chaoxing.com/',
+        }
+        self.ac()
+
+    def ac(self):
+        con = 0
+        while True:
+            if con == 6:
+                break
+            try:
+                self.login()
+                self.get_token()
+                break
+            except Exception as e:
+                con += 1
+                print('error!', self.acc, e)
+                self.session.close()
+                self.session = requests.session()
+                continue
+            finally:
+                pass
+
+    def login(self):
+        c_url = 'https://passport2.chaoxing.com/mlogin?' \
+                'loginType=1&' \
+                'newversion=true&fid=&' \
+                'refer=http%3A%2F%2Foffice.chaoxing.com%2Ffront%2Fthird%2Fapps%2Fseat%2Findex'
+        self.session.get(c_url).cookies.get_dict()
+        data = {
+            'fid': '-1',
+            'uname': self.acc,
+            'password': base64.b64encode(self.pwd.encode()).decode(),
+            'refer': 'http%3A%2F%2Foffice.chaoxing.com%2Ffront%2Fthird%2Fapps%2Fseat%2Findex',
+            't': 'true'
+        }
+        self.session.post('https://passport2.chaoxing.com/fanyalogin', data=data)
+        s_url = 'https://office.chaoxing.com/front/third/apps/seat/index'
+        self.session.get(s_url)
+
+    def get_token(self):
+        response = self.session.get(url=f'https://office.chaoxing.com/front/apps/seat/list')
+        self.pageToken = re.compile(r"&pageToken=' \+ '(.*)' \+ '&").findall(response.text)[0]
+        response = self.session.get(url='https://office.chaoxing.com/front/apps/seat/select?'
+                                        f'id={self.roomid}&'
+                                        f'day={self.day}&'
+                                        'backLevel=2&'
+                                        f'pageToken={self.pageToken}')
+        self.token = re.compile("token: '(.*)'").findall(response.text)[0]
+
+    def submit(self, start, end):
+        """
+        预约方法
+        @param start: 开始时间
+        @param end: 结束时间
+        """
+        while True:
+            self.captcha = self.get_captcha()
+            if self.captcha:
+                break
+        response = self.session.get(url='https://office.chaoxing.com/data/apps/seat/submit?'
+                                        f'roomId={self.roomid}&'
+                                        f'startTime={start}&'
+                                        f'endTime={end}&'
+                                        f'day={self.day}&'
+                                        f'seatNum={self.seatnums}&'
+                                        f'captcha={self.captcha}&'
+                                        f'token={self.token}')
+        print(self.acc, response.json())
+
+    def get_captcha(self):
+        """
+        处理滑块验证码
+        @return: 百分之七十五的准确率预约captcha值
+        """
+        jq = 'jQuery' + randomNum(16) + '_' + str(time.time() * 1000)[:13]
+        t = int(str(time.time() * 1000)[:13])
+        res = self.session.get('https://captcha.chaoxing.com/captcha/get/verification/image?'
+                               f'callback=jQuery{randomNum(16)}_{str(time.time() * 1000)[:13]}&'
+                               'captchaId=42sxgHoTPTKbt0uZxPJ7ssOvtXr3ZgZ1&type=slide&version=1.1.6'
+                               f'&_={str(t)}').text
+        result = res.split('(')[1].split(')')[0]
+        res = json.loads(result)
+        token_captcha = res['token']
+        shadeImage = res['imageVerificationVo']['shadeImage']  # 'data:image/png;base64,' +
+        cutoutImage = res['imageVerificationVo']['cutoutImage']
+        textClickArr = captchaCv.main(base64.b64decode(cutoutImage), base64.b64decode(shadeImage))[1]
+        textClickArr = '[{"x":' + str(textClickArr) + '}]'
+        params = (
+            ('callback', jq),
+            ('captchaId', '42sxgHoTPTKbt0uZxPJ7ssOvtXr3ZgZ1'),
+            ('type', 'slide'),
+            ('token', token_captcha),
+            ('textClickArr', textClickArr),
+            ('coordinate',
+             '[]'),
+            ('runEnv', '10'),
+            ('version', '1.1.6'),
+            ('_', str(t + 1)),
+        )
+        res = self.session.get(url='https://captcha.chaoxing.com/captcha/check/verification/result', params=params).text
+        res = res.split('(')[1].split(')')[0]
+        res = json.loads(res)
+        if res["result"]:
+            return json.loads(res['extraData'])["validate"]
+        return False
+
+
+def diy_book(starttime, endtime):
+    """
+    取时间段按两小时切片
+    @param starttime: 开始时间
+    @param endtime: 结束时间
+    @return: 时间列表
+    """
+    start_h, start_m = starttime.split(':', 1)
+    end_h, end_m = endtime.split(':', 1)
+    ls_time = []
+    start = int(start_h)
+    for c in range(int(end_h) - int(start_h)):
+        if (int(end_h) - start) <= 2:
+            ls_time.append([str(start) + ':' + start_m, end_h + ':' + end_m])
+            break
+        ls_time.append([str(start) + ':' + start_m, str(int(start) + 2) + ':' + start_m])
+        start += 2
+    return ls_time
+
+
+def submit_sub(seatnums, roomid, start, end, acc, pwd):
+    """
+    预约子方法
+    @param seatnums: 座位号
+    @param roomid: 房间号
+    @param start: 开始时间
+    @param end: 结束时间
+    @param acc: 账号
+    @param pwd: 密码
+    """
+    sub = CX(acc, pwd, roomid=roomid, seatnums=seatnums)
+    for index_a in diy_book(start, end):
+        sub.submit(start=index_a[0], end=index_a[1])
+
+
+def book_seat(list_form):
+    """
+    多账号多线程预约支持
+    @param list_form: 座位号 房间号 开始时间 结束时间 手机号 密码
+    """
+    ths = []
+    for index1 in list_form:
+        ths.append(
+            threading.Thread(target=submit_sub,
+                             args=(index1[0], index1[1], index1[2], index1[3], index1[4], index1[5],))
+        )
+    for th in ths:
+        # print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"), 'start_thread')
+        th.start()
+    for th in ths:
+        th.join()
+
+
+def randomNum(num):
+    """
+    产生num+4位的随机数
+    @param num: 数量
+    @return: nums+4位的随机数
+    """
+    l_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    nums = '3310'
+    for index in range(num):
+        nums += str(random.choice(l_nums))
+    return nums
+
+
+if __name__ == '__main__':
+    a = time.time()
+    lis = [
+        ['081', '6101', '9:00', '21:00', '1********', '****']  # 座位号 房间号 开始时间 结束时间 手机号 密码
+    ]
+    book_seat(lis)  # 立刻预约
+    print(time.time() - a)
---
+++ SSA_captcha/captchaCv.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+
+# 获取滑块的大小
+def fix_img(filename):
+    img = cv2.imdecode(np.array(bytearray(filename), dtype='uint8'), cv2.IMREAD_UNCHANGED)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contour_img = cv2.drawContours(img, contours, 0, (0, 255, 0), 1)
+    x, y, w, h = cv2.boundingRect(contours[0])
+    mixintu = contour_img[y:y + h, x:x + w]
+    return mixintu
+
+
+def main(fadebg, fullbg):
+    """
+    处理滑块验证码
+    @param fadebg: 滑块图
+    @param fullbg: 背景图
+    @return: 'cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED'的对比结果以及最佳位移值
+    """
+    tp_img = fix_img(fadebg)
+    tp_edge = cv2.Canny(tp_img, 100, 200)
+    tp_pic = cv2.cvtColor(tp_edge, cv2.COLOR_GRAY2BGR)
+    bg_img = cv2.imdecode(np.array(bytearray(fullbg), dtype='uint8'), cv2.IMREAD_UNCHANGED)
+    bg_edge = cv2.Canny(bg_img, 100, 200)
+    bg_pic = cv2.cvtColor(bg_edge, cv2.COLOR_GRAY2BGR)
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED']
+    results = {}
+    move = 0
+    for meth in methods:
+        temp_full_img = bg_pic.copy()
+        res = cv2.matchTemplate(temp_full_img, tp_pic, eval(meth))
+        loc = cv2.minMaxLoc(res)[3]
+        results[meth] = loc
+        move = loc[0]
+    return results, move
